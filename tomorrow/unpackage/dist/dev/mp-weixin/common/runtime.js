65b89ea84f65c978b4721b9b122cab6a21a70179
HP <215572162@qq.com>
2020-10-30 15:29:29 +0800

    trial_push

--- tomorrow/unpackage/dist/dev/mp-weixin/common/runtime.js
+++ tomorrow/unpackage/dist/dev/mp-weixin/common/runtime.js
@@ -105,11 +105,11 @@
 /******/
 /******/
 /******/ 		// mini-css-extract-plugin CSS loading
-/******/ 		var cssChunks = {"components/index":1};
+/******/ 		var cssChunks = {"components/today":1};
 /******/ 		if(installedCssChunks[chunkId]) promises.push(installedCssChunks[chunkId]);
 /******/ 		else if(installedCssChunks[chunkId] !== 0 && cssChunks[chunkId]) {
 /******/ 			promises.push(installedCssChunks[chunkId] = new Promise(function(resolve, reject) {
-/******/ 				var href = "" + ({"components/index":"components/index"}[chunkId]||chunkId) + ".wxss";
+/******/ 				var href = "" + ({"components/today":"components/today"}[chunkId]||chunkId) + ".wxss";
 /******/ 				var fullhref = __webpack_require__.p + href;
 /******/ 				var existingLinkTags = document.getElementsByTagName("link");
 /******/ 				for(var i = 0; i < existingLinkTags.length; i++) {
